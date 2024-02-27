b253b1b2ada46fda25d2fe3d95a1e89888c28ba3
Alan Felipe <felipemoreira.satelite@gmail.com>
2024-02-27 09:00:24 -0300

    test(git-python): funcao sub teste

--- teste_calculadora.py
+++ teste_calculadora.py
@@ -4,5 +4,6 @@ def main():
     b = 3
     soma = calculadora.somar(a, b)
     print(f'(a) + (b) = (soma)')
-
+    subtrair = calculadora.subtrair(a, b)
+    print(f'(a) - (b) = (subtrair)')
 main()    
