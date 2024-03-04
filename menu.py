691814f3eb25d161e2d4c820217939fdf60a0e09
Guilherme Matheus <guilherme.sou@hotmail.com>
2024-03-04 20:40:08 -0300

    ap01(git-python): opção subtrair implementada

--- menu.py
+++ menu.py
@@ -4,6 +4,7 @@ def menu():
     while True:
         print("\nEscolha uma opção:")
         print("1 para Somar")
+        print("2 para Subtrair")
         print("0 para Sair")
 
         opcao = input("Digite o número da opção desejada: ")
@@ -12,6 +13,11 @@ def menu():
             a = float(input("Digite o primeiro número: "))
             b = float(input("Digite o segundo número: "))
             print("Resultado:", calculadora.somar(a, b))
+        
+        elif opcao == '2':
+            a = float(input("Digite o primeiro número: "))
+            b = float(input("Digite o segundo número: "))
+            print("Resultado:", calculadora.subtrair(a, b))
             
         elif opcao == '0':
             print("Saindo...")
