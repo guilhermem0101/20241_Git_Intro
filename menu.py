c665b9263a55c15b13a8d92886430146f315ff4a
Guilherme Matheus <guilherme.sou@hotmail.com>
2024-03-04 20:41:03 -0300

    ap01(git-python): opção multiplicar implementada

--- menu.py
+++ menu.py
@@ -5,6 +5,7 @@ def menu():
         print("\nEscolha uma opção:")
         print("1 para Somar")
         print("2 para Subtrair")
+        print("3 para Multiplicar")
         print("0 para Sair")
 
         opcao = input("Digite o número da opção desejada: ")
@@ -18,6 +19,11 @@ def menu():
             a = float(input("Digite o primeiro número: "))
             b = float(input("Digite o segundo número: "))
             print("Resultado:", calculadora.subtrair(a, b))
+        
+        elif opcao == '3':
+            a = float(input("Digite o primeiro número: "))
+            b = float(input("Digite o segundo número: "))
+            print("Resultado:", calculadora.multiplicar(a, b))
             
         elif opcao == '0':
             print("Saindo...")
